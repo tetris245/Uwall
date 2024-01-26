3dbf19d6c41222fd2ed5886376e344ea51dd3280
tetris245 <58624501+tetris245@users.noreply.github.com>
2024-01-26 18:03:13 +0100

    Update Uwallbookmark.js

--- Uwallbookmark.js
+++ Uwallbookmark.js
@@ -1,1 +1,1 @@
-javascript:(function (){let n = document.createElement("script");n.setAttribute("language", "JavaScript");n.setAttribute("crossorigin", "anonymous");n.setAttribute("src", "https://tetris245.github.io/Uwall.js?_=" + Date.now());document.head.appendChild(n);})();
+javascript:(()=>{fetch(`https://tetris245.github.io/Uwall.user.js?_=${Date.now()}`).then(r=>r.text()).then(r=>eval(r));})();
